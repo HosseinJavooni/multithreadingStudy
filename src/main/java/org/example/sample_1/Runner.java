203dffe624a0b47188d534e1c8b2c9a6c498cdf0
Hossein Jabani <h.javooni@faraboom.co>
2024-07-01 15:08:10 +0330

    master

--- src/main/java/org/example/sample_1/Runner.java
+++ src/main/java/org/example/sample_1/Runner.java
@@ -1,4 +1,14 @@
 package org.example.sample_1;
 
-public class Runner {
+public class Runner extends Thread{
+    private final String identifier;
+    public Runner(String identifier){
+        this.identifier = identifier;
+    }
+    @Override
+    public void run(){
+        for(int i=0; i < 1000; i++) {
+            System.out.print(identifier);
+        }
+    }
 }
--- src/main/java/org/example/sample_1/sample_1_main.java
+++ src/main/java/org/example/sample_1/sample_1_main.java
@@ -1,4 +1,10 @@
 package org.example.sample_1;
 
 public class sample_1_main {
+    public static void main(String[] args) {
+        Runner thread1 = new Runner("0");
+        Runner thread2 = new Runner("_");
+        thread1.start();
+        thread2.start();
+    }
 }
